56c1037ab60942ecece6d37168dba4663c2e9868
udara-lakshitha <lakshithau95@gmail.com>
2025-06-30 23:13:23 +0530

    feat: Solve Basic I - Problem 1

---
+++ 01-Python-Basic-Part-I/problem_001.py
@@ -0,0 +1,11 @@
+# 1. Formatted Twinkle Poem
+
+poem = '''
+Twinkle, twinkle, little star,
+	How I wonder what you are! 
+		Up above the world so high,   		
+		Like a diamond in the sky. 
+Twinkle, twinkle, little star, 
+	How I wonder what you are
+'''
+print(poem)
