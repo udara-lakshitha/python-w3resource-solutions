ccd5d2e674f3fa8d26eb7ca69cf4e9ddd965238b
udara-lakshitha <lakshithau95@gmail.com>
2025-07-01 00:36:08 +0530

    feat: Solve Basic I - Problem 8

---
+++ 01-Python-Basic-Part-I/problem_008.py
@@ -0,0 +1,5 @@
+# 8. First and Last Colors
+
+# The color list is given in the problem. Therefore, I used exact same list
+color_list = ["Red","Green","White" ,"Black"]
+print(f'First color is: {color_list[0]} and last color is: {color_list[-1]}')
