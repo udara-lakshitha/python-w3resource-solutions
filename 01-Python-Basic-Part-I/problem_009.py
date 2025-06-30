106437b9655131d8b10d19e44be3671c91e57eb1
udara-lakshitha <lakshithau95@gmail.com>
2025-07-01 00:45:50 +0530

    feat: Solve Basic I - Problem 9

---
+++ 01-Python-Basic-Part-I/problem_009.py
@@ -0,0 +1,6 @@
+# 9. Exam Schedule Formatter
+
+# The exam_st_date is used for this solution. Didn't generalize the solution.
+exam_st_date = (11, 12, 2014)
+day, month, year = exam_st_date
+print(f'The examination will start from: {day}/{month}/{year}')
